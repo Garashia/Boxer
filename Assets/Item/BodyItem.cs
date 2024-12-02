3a4ada3e1a61d44b05d2282ba699c6a5f0a76329
No Name <117160460+Garashia@users.noreply.github.com>
2024-12-02 15:41:55 +0900

    奥の細道

--- Assets/Item/BodyItem.cs
+++ Assets/Item/BodyItem.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ItemTable", menuName = "ScriptableObjects/Item/BodyItem")]
 public class BodyItem : Item
 {
+    [SerializeField]
+    private int m_luck;
 
+    void Uni()
+    {
+    }
 
-
-
+    public override ItemType GetItemType() { return ItemType.Body; }
 }
